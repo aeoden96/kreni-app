b16701ce582b4f763c73156cfd1a9e4a52271047
mateo <mateo1559@gmail.com>
2026-03-10 02:23:21 +0100

    feat: remove CacheSizeIndicator component from AppLayout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { SpiderMenu } from '../components/Navigation/SpiderMenu';
 import { UpdatePrompt } from '../components/common/UpdatePrompt';
-import { CacheSizeIndicator } from '../components/common/CacheSizeIndicator';
 
 export function AppLayout() {
     return (
@@ -9,16 +8,8 @@ export function AppLayout() {
             <div className="flex-1 relative overflow-hidden">
                 <Outlet />
             </div>
-            {/* 
-                SpiderMenu is global. On the Map page (/), it will also show 
-                the Locate button because we'll pass props from App.tsx 
-                or handle it via route checking if we move it.
-                Actually, let's keep it here but we'll need a way for App.tsx 
-                to 'register' its locate function.
-            */}
             <SpiderMenu />
             <UpdatePrompt />
-            <CacheSizeIndicator />
         </div>
     );
 }
